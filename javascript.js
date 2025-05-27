2bb3c8cdacb58158c89e2c8acd15011ffa5fdd2e
Hwang DoYeon <150096210+wowyaydough@users.noreply.github.com>
2025-05-27 21:21:39 +0000

    change the style and js of alert box

--- javascript.js
+++ javascript.js
@@ -1,5 +1,13 @@
-const openAlert = () => {
-    document.querySelector(".alert-box").style.display="block"
+const controlAlert = (status,btnControl) => {
+    document.querySelector(".alert-box").style.display=status;
+
+    if(btnControl === 'false'){
+        document.querySelector('.btn_open').style.display='none'
+    }
+    else{
+        document.querySelector('.btn_open').style.display='block'
+    }
 }
 
-document.querySelector(".open").addEventListener("click", openAlert)
+document.querySelector(".btn_open").addEventListener("click", () => controlAlert('flex','false'));
+document.querySelector('.btn_close').addEventListener('click', () => controlAlert('none','true'));
